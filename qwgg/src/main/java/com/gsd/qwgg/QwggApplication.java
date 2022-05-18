57a11071dc725f015e38fc87e55d906e4f18f88c
Yohan Lee <swj03095@naver.com>
2022-05-18 23:19:30 +0900

    init

---
+++ qwgg/src/main/java/com/gsd/qwgg/QwggApplication.java
@@ -0,0 +1,13 @@
+package com.gsd.qwgg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QwggApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QwggApplication.class, args);
+    }
+
+}
---
+++ qwgg/src/test/java/com/gsd/qwgg/QwggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsd.qwgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QwggApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
