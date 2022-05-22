6b92368aa6e83a7dbef204ea29902177c0831ae0
Yohan Lee <swj03095@naver.com>
2022-05-22 19:01:21 +0900

    gradle 멀티모듈 셋팅

---
+++ qwqwgg-core/src/main/java/com/gsd/qwgg/core/util/TestUtil.java
@@ -0,0 +1,12 @@
+package com.gsd.qwgg.core.util;
+
+/**
+ * Created by Yohan lee Created on 2022/05/22.
+ **/
+public class TestUtil {
+
+
+    public static String str() {
+        return "a";
+    }
+}
---
+++ qwqwgg-web/src/main/java/com/gsd/qwgg/web/QwggWebApplication.java
@@ -0,0 +1,13 @@
+package com.gsd.qwgg.web;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QwggWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QwggWebApplication.class, args);
+    }
+}
